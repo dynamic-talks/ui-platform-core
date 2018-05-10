7aebe1a369328e54b3e4a25e86522f0d72aeb830
Igor Tomov <itomov@griddynamics.com>
2018-05-10 15:53:57 +0300

    Added build process with Gulp

---
+++ Gulpfile.js
@@ -0,0 +1,18 @@
+const gulp  = require('gulp');
+const del   = require('del');
+const babel = require('gulp-babel-compile');
+
+
+/**
+ * Build sources into `dist` folder
+ */
+gulp.task('build', () => {
+  // remove previous build
+  del.sync(['./dist']);
+
+  // compile src with Babel and put it into `dist` folder
+  return gulp
+    .src('./lib/**/*.js')
+    .pipe(babel())
+    .pipe(gulp.dest('./dist/lib'))
+})
