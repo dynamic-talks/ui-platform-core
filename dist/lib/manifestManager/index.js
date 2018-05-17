1419fea9bbeb9573ba66665caf7e1ef1a3215efa
lidak <animatio.dev@gmail.com>
2018-05-17 15:57:07 +0300

    PR comments fixed.

--- dist/lib/manifestManager/index.js
+++ dist/lib/manifestManager/index.js
@@ -15,24 +15,21 @@ var ManifestManager = exports.ManifestManager = function () {
     if (!manifestPath) {
       throw 'Can\'t init ManifestManger instance without passing manifestPath as a param.';
     }
-    this.manifestPath = manifestPath;
-    this.setManifest();
+    this.loadManifest(manifestPath);
   }
 
   _createClass(ManifestManager, [{
-    key: 'setManifest',
-    value: function setManifest() {
+    key: 'loadManifest',
+    value: function loadManifest(manifestPath) {
       if (!this._manifest) {
-        this._manifest = require(this.manifestPath);
+        this._manifest = require(manifestPath);
       }
-
-      console.log('Manifest successfully has been set. ' + this._manifest);
     }
   }, {
     key: 'get',
     value: function get(manifestProp) {
       if (!this._manifest) {
-        return console.error('Trying to get ' + manifestProp + ' prop of not existed manifest. Try to call setManifest method on an instance.');
+        return console.error('Trying to get ' + manifestProp + ' prop of not existed manifest. Try to call loadManifest method on an instance.');
       }
 
       return this._manifest[manifestProp] || console.error('There\'s no ' + manifestProp + ' prop found. ' + Object.keys(this._manifest));
